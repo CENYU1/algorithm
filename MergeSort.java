cd62852e9f6511be7bdd44fc04370a26b4c6a413
cenyu <kcenyu10@126.com>
2022-08-17 18:01:54 +0800

    添加了有关mergeSort的注释

--- MergeSort.java
+++ MergeSort.java
@@ -1,4 +1,12 @@
 public class MergeSort {
+    /*
+     * 归并排序 O(NlogN) 
+     * 为什么比冒泡排序、插入排序、选择排序好，就是因为归并排序不浪费比较次数
+     * master公式 T(N) = 2T(N/2) + O(N)
+     * 对于merge()方法，每次都是从左到右遍历一次数组，因此是O(N)
+     * a = 2, b = 2, c = 1
+     * 因此时间复杂度为 O(NlogN)
+     */
     public static void mergeSort(int[] arr) {
         if(arr == null || arr.length < 2) return;
         process(arr, 0, arr.length - 1);
